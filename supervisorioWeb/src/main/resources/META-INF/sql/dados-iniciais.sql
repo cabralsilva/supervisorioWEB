d6d5fa98885c7458ed6d494914da1a6fe68e39ce
Daniel Nadson <d.nadson@yahoo.com.br>
2019-01-31 10:47:42 -0200

    first commit

---
+++ supervisorioWeb/src/main/resources/META-INF/sql/dados-iniciais.sql
@@ -0,0 +1,5 @@
+
+INSERT [dbo].[Empresa] ([cnpj], [data_fundacao], [nome_fantasia], [razao_social], [tipo]) VALUES (N'70.311.193/0001-87', CAST(N'2009-02-03T00:00:00.000' AS DateTime), N'Mercado do João', N'João Mercado e Distribuidor de Alimentos Ltda', N'LTDA')
+INSERT [dbo].[Empresa] ([cnpj], [data_fundacao], [nome_fantasia], [razao_social], [tipo]) VALUES (N'52.822.994/0001-25', CAST(N'1997-10-12T00:00:00.000' AS DateTime), N'Fale Mais', N'Fale Mais Telecom S.A.', N'SA')
+INSERT [dbo].[Empresa] ([cnpj], [data_fundacao], [nome_fantasia], [razao_social], [tipo]) VALUES (N'41.952.519/0001-57', CAST(N'2014-10-15T00:00:00.000' AS DateTime), N'Maria de Souza da Silva', N'Maria de Souza da Silva', N'MEI')
+INSERT [dbo].[Empresa] ([cnpj], [data_fundacao], [nome_fantasia], [razao_social], [tipo]) VALUES (N'16.134.777/0001-89', CAST(N'2019-03-02T00:00:00.000' AS DateTime), N'Gomes Inovação', N'José Fernando Gomes EIRELI ME', N'EIRELI')
